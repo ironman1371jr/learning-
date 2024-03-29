2b9a4c8ee66ef2eb991c07be9e67f3cc0ea8d173
ashvind kumar <112551583+ironman1371jr@users.noreply.github.com>
2024-03-30 01:11:00 +0530

    Update cd.py

--- scripting/os/cd.py
+++ scripting/os/cd.py
@@ -1,3 +1,24 @@
 import os
 
-print(os.getcwd())   # print the current working directory
+print(os.getcwd())    # print the current working directory
+
+os.chdir("D:\codes\scripting") # change to a new directory, if it exists 
+
+def file_exists(filename):
+    if os.path.exists(filename):
+        print("file exists")
+    else:
+        print("does not exist")
+
+file_exists("D:\codes\scripting")
+
+def listall(directory):
+    if  os.path.isdir(directory):
+        filenames = os.listdir(directory)
+        for file in filenames:
+            print(file)
+        print(os.listdir(directory))
+    else:
+        print("not a valid directory")
+
+listall("D:\codes\scripting\os")# print the current working directory
